57960eb5e204b76b2f445e0d6fc71eed9e925929
Birukou Aliaksei <Birukou.aliaksei@gmail.com>
2021-03-11 18:20:23 +0300

    added car logo

--- Car Card/force-app/main/default/lwc/tile/tile.js
+++ Car Card/force-app/main/default/lwc/tile/tile.js
@@ -1,17 +1,25 @@
 import { LightningElement, api } from 'lwc';
 // import {products} from "c/list"
+import LOGO from '@salesforce/resourceUrl/CarDreamLogo';
 
 export default class Tile extends LightningElement {
     //TODO add product id
     @api product;
 
+    sourceUrl = LOGO;
+    
+    check () {
+        console.log(product);
+    }
+
     tileClick() {
         console.log();
         const event = new CustomEvent('tileclick', {
             // detail contains only primitives
-            detail: this.product.Id
+            detail: this.product.Name
         });
         console.log();
+        console.log(detail);
         // Fire the event from c-tile
         this.dispatchEvent(event);
     }
