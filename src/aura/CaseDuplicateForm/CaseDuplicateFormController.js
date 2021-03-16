0a8a6983d7b4d8149ac153fe882063d9b1c555e9
Birukou Aliaksei <Birukou.aliaksei@gmail.com>
2021-03-16 19:38:56 +0300

    added recordId

--- src/aura/CaseDuplicateForm/CaseDuplicateFormController.js
+++ src/aura/CaseDuplicateForm/CaseDuplicateFormController.js
@@ -1,31 +1,6 @@
 /**
- * Created by Bruss on 3/2/2021.
+ * Created by Bruss on 3/14/2021.
  */
 
 ({
-        getCasesList: function (component, event) {
-            let action = component.get("c.getCases");
-
-            action.setCallback(this, function (response) {
-                let state = response.getState();
-                if (state === "SUCCESS") {
-                    component.set("v.cases", response.getReturnValue());
-
-                } else {
-                    console.log("Failed with state: " + state);
-                }
-            });
-            $A.enqueueAction(action);
-        },
-
-        mergeCasesList: function (component, event) {
-            console.info(component);
-            let openModal = component.getEvent("openModal");
-            openModal.setParams({"openPopup" : true});
-            console.info();
-            openModal.fire();
-            // component.set("v.isModalOpen", true);
-        },
-    }
-
-)
+});
