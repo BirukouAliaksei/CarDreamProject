23a43b5df9e2ec90444358b9cc6ff2616ea195f2
Birukou Aliaksei <Birukou.aliaksei@gmail.com>
2021-03-17 18:32:34 +0300

    added car list

--- src/aura/CarList/CarListController.js
+++ src/aura/CarList/CarListController.js
@@ -3,4 +3,48 @@
  */
 
 ({
+    doInit: function (component, event, helper) {
+        let action = component.get("c.getAllProducts");
+        action.setCallback(this, function (response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.products", response.getReturnValue());
+            } else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    searchKeyChange: function (component, event) {
+        let searchKey = component.find("searchKey").get("v.value");
+        console.log('searchKey:::::' + searchKey);
+        let action = component.get("c.findProductsByKey");
+        action.setParams({
+            "searchKey": searchKey
+        });
+        action.setCallback(this, function (a) {
+            component.set("v.products", a.getReturnValue());
+        });
+        $A.enqueueAction(action);
+    },
+
+    showCar: function (component, event, helper) {
+        let car = event.getSource().get("v.name");
+        console.log("Show productNew: " + JSON.stringify(car));
+
+        let action = component.get("c.getProductById");
+        action.setParams({"productId": car});
+        action.setCallback(this, function (response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                console.log(response.getReturnValue());
+                component.set("v.productNew", response.getReturnValue());
+            } else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        $A.enqueueAction(action);
+
+    }
 });
--- src/aura/CarList/CarListHelper.js
+++ src/aura/CarList/CarListHelper.js
@@ -3,4 +3,11 @@
  */
 
 ({
+    createProduct: function (component, product) {
+        let theProduct = component.get("v.products");
+        let productNew = JSON.parse(JSON.stringify(product));
+        theProduct.push(productNew);
+        component.set("v.products", theProduct);
+    }
+
 });
